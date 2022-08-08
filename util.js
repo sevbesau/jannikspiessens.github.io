2a224101beb35dbeddaf1112073cd2a508f69def
sevbesau <seppe@sevbesau.xyz>
2022-08-08 17:17:45 +0200

    added tailwind and nice styles. Also a bit of code cleanup :)

--- util.js
+++ util.js
@@ -1,7 +1,48 @@
+// set the inner html and execute script tags if there are any
+function set_innerHTML(elm, html) {
+  elm.innerHTML = html;
+  Array.from(elm.querySelectorAll("script")).forEach((oldScript) => {
+    const newScript = document.createElement("script");
+    Array.from(oldScript.attributes).forEach((attr) =>
+      newScript.setAttribute(attr.name, attr.value)
+    );
+    newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+    oldScript.parentNode.replaceChild(newScript, oldScript);
+  });
+}
+
+// set the content for a stage
+function show_stage(content, stageNr) {
+  set_innerHTML(document.getElementById("text"), content);
+  document.getElementById("lvl_nr").value = "";
+  document.getElementById("pass").value = "";
+  document.getElementById("lvl_tracker").innerHTML = `stage: ${stageNr}`;
+  set_error("");
+}
+
+// set the error content and show/hide it
+function set_error(content, show = false) {
+  const error = document.getElementById("error");
+  error.innerHTML = content;
+  if (show) error.classList.remove("hidden");
+  else error.classList.add("hidden");
+}
+
+// unlock the next stage
+async function unlock_stage(stageNr, password) {
+  const passwordHash = await sha256(await sha256(password));
+  const res = await fetch(`stages/${stageNr}.html`);
+  const stage = await res.text();
+  const [stageHash, stageContents] = stage.split("\n\n");
+  if (stageHash != passwordHash) return set_error("Invalid password", true);
+  const decryptedContent = await aesGcmDecrypt(stageContents, password);
+  show_stage(decryptedContent, stageNr)
+}
+
 // hash digest (https://jameshfisher.com/2017/10/30/web-cryptography-api-hello-world/)
 async function sha256(str) {
   const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
-  return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+  return Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('');
 }
 
 // encrypt/decrypt AES-GCM (https://gist.github.com/chrisveness/43bcda93af9f646d083fad678071b90a)
@@ -24,14 +65,14 @@ async function aesGcmEncrypt(plaintext, password) {
 
   const ivHex = Array.from(iv).map(b => ('00' + b.toString(16)).slice(-2)).join('');
 
-  return ivHex+ctBase64;
+  return ivHex + ctBase64;
 }
 
 async function aesGcmDecrypt(ciphertext, password) {
   const pwUtf8 = new TextEncoder().encode(password);
   const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
 
-  const iv = ciphertext.slice(0,24).match(/.{2}/g).map(byte => parseInt(byte, 16));
+  const iv = ciphertext.slice(0, 24).match(/.{2}/g).map(byte => parseInt(byte, 16));
 
   const alg = { name: 'AES-GCM', iv: new Uint8Array(iv) };
 
@@ -45,10 +86,3 @@ async function aesGcmDecrypt(ciphertext, password) {
 
   return plaintext;
 }
-
-// show an error
-function show_error(msg) {
-  var err = '<span id="err", style="background-color:red;color:white;">'+msg+"</span>";
-  $("body").html($("body").html()+err);
-}
-
